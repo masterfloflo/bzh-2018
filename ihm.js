32993ca616f0f1598ebde7e4e16c01c9ea056211
masterfloflo <43879013+masterfloflo@users.noreply.github.com>
2018-11-28 13:41:41 +0100

    OKK

--- ihm.js
+++ ihm.js
@@ -10,33 +10,33 @@ exports.start = function () {
 
     var readline = require('readline');
 
-var menu = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+    var menu = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
 
 
-var menumenu = menu.question("", function (saisie) {
-    if (`${saisie}` == 1) {
-        service.init(function(nb) {
-            console.log   (nb, 'Données mises à jour') 
-               }), 
-        menu.close();
+    var menumenu = menu.question("", function (saisie) {
+        if (`${saisie}` == 1) {
+            service.init(function (nb) {
+                console.log(nb, 'Données mises à jour')
+            }),
+                menu.close();
 
-    }
-    else if (`${saisie}` == 2) {
-        service.listerSessions(function (nb) {
-            console.log(nb, 'voici la liste')
-        })
-        menu.close();
+        }
+        else if (`${saisie}` == 2) {
+            service.listerSessions(function (nb) {
+                console.log(nb, 'voici la liste')
+            })
+            menu.close();
 
-    }
-    else if (`${saisie}` == 99) {
-        menu.close();
-    }
-    else {
-        console.log("Ce chiffre ne renvoie rien, try again")
-    }
-})
+        }
+        else if (`${saisie}` == 99) {
+            menu.close();
+        }
+        else {
+            console.log("Ce chiffre ne renvoie rien, try again")
+        }
+    })
 
 }
